0e4c76921acbd1535d5ad14813dc68793d4a6c4b
Antonio-Puca <antoniopuca.work@gmail.com>
2026-06-07 09:59:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const viaggi =
+  document.querySelectorAll("#offerta-settimana .card").length +
+  document.querySelectorAll("#welcome-summer .card").length +
+  document.querySelectorAll("#last-minute .card").length;
+
+document.getElementById("contatore-viaggi").textContent =
+  "Destinazioni disponibili: " + viaggi;
+
+const bottone = document.getElementById("toggle-last-minute");
+const contenuto = document.getElementById("contenuto-last-minute");
+
+bottone.addEventListener("click", () => {
+  contenuto.classList.toggle("d-none");
+
+  if (contenuto.classList.contains("d-none")) {
+    bottone.textContent = "Mostra Offerte Lampo";
+  } else {
+    bottone.textContent = "Chiudi Offerte Lampo";
+  }
+});
+
+const eliminaBtn = document.getElementById("rimuovi-card");
+
+eliminaBtn.addEventListener("click", () => {
+  const cards = document.querySelectorAll(
+    "#offerta-settimana .card, #welcome-summer .card, #last-minute .card",
+  );
+
+  cards.forEach((card) => {
+    card.classList.toggle("d-none");
+  });
+
+  if (eliminaBtn.textContent === "Nascondi Destinazioni") {
+    eliminaBtn.textContent = "Mostra Destinazioni";
+  } else {
+    eliminaBtn.textContent = "Nascondi Destinazioni";
+  }
+});
